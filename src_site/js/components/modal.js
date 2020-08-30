0327415bb2421052067c82080e0a2da7ead28d44
dGrammatiko <d.grammatiko@gmail.com>
2020-08-30 18:18:45 +0200

    Modal animation

--- src_site/js/components/modal.js
+++ src_site/js/components/modal.js
@@ -1,6 +1,5 @@
 import { render, html } from "uhtml"
 
-let focusableElements = [];
 const KEYCODE = {
   TAB: 9,
   ESC: 27,
@@ -9,59 +8,56 @@ const KEYCODE = {
 function modalEncapsulation(modal) {
   modal.style.display = 'flex';
   modal.style.visibility = 'visible';
-  focusableElements = [].slice.call(modal.querySelectorAll(['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex^="-"])'].join()));
+  modal.focusableElements = [].slice.call(modal.querySelectorAll(['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex^="-"])'].join()));
 
-  if (focusableElements.length) {
-    focusableElements[0].focus();
+  console.log(modal.focusableElements)
+  if (modal.focusableElements.length) {
+    modal.focusableElements[0].focus();
   }
 
   function keycontrol(e) {
+    debugger;
     // ESC key
     if (e.keyCode === KEYCODE.ESC) {
       console.log(modal)
       modal.style.display = 'none';
       modal.style.visibility = 'hidden';
-      document.removeEventListener(modal, keycontrol);
+      modal.removeEventListener(modal, keycontrol);
     }
 
     // TAB key
     if (e.keyCode === KEYCODE.TAB) {
       // this.handleTabEvent(e);
       // Get the index of the current active element within the modal
-      const focusedIndex = focusableElements.indexOf(document.activeElement);
+      const focusedIndex = modal.focusableElements.indexOf(document.activeElement);
+      console.log(focusedIndex)
       // Handle TAB event if need to skip
       // If first element is focused and shift key is in use
       if (e.shiftKey && (focusedIndex === 0 || focusedIndex === -1)) {
         // Focus last item within modal
-        focusableElements[focusableElements.length - 1].focus();
+        modal.focusableElements[modal.focusableElements.length - 1].focus();
         e.preventDefault();
       }
       // If last element is focused and shift key is not in use
-      if (!e.shiftKey && focusedIndex === focusableElements.length - 1) {
+      if (!e.shiftKey && focusedIndex === modal.focusableElements.length - 1) {
         // Focus first item within modal
-        focusableElements[0].focus();
+        modal.focusableElements[0].focus();
         e.preventDefault();
       }
     }
   };
 
-  document.addEventListener('keyup', keycontrol);
-
-  // window.addEventListener('click', (event) => {
-  //   const clickedEl = event.target
-  //   console.log(clickedEl)
-  //   // if (clickedEl.closest('.modal') !== modal) {
-  //   //   modal.parentNode.innerHTML = ''
-  //   // }
-  // });
+  modal.keycontrol = keycontrol;
+  modal.addEventListener('keydown', modal.keycontrol);
   console.log(modal)
 }
 
-function closeModal(e) {
+function closeModal(event) {
   let element = event.target.closest('.light-modal')
   if (element) {
     element.style.display = 'none';
     element.style.visibility = 'hidden';
+    element.removeEventListener(element, element.keycontrol);
   }
 }
 
@@ -71,7 +67,7 @@ export const renderModal = (where, data) => {
     where,
     html`
 <div class="light-modal" ref=${modalEncapsulation} role="dialog" aria-labelledby="light-modal-label" aria-hidden="false">
-  <div class="light-modal-content animated zoomInUp">
+  <div class="light-modal-content animated fadeInUp">
     <div class="light-modal-header">
       <h3 class="light-modal-heading">${data.title}</h3>
       <button class="light-modal-close-icon" aria-label="close" onclick=${closeModal}>&times;</button>
@@ -81,14 +77,14 @@ export const renderModal = (where, data) => {
       <picture class="card-image">
         <img loading="lazy" src=${image} />
       </picture>
-
-        <ul>
-          <li>Performace: <span>${data.metrics.performance}%</span></li>
-          <li>First contentful paint:<span>${data.metrics.firstContentfulPaint.toFixed(2)}Sec </span></li>
-          <li>Best practices: <span>${Math.round(data.metrics.bestPractices)}%</span></li>
-          <li>Accessibility:<span>${Math.round(data.metrics.accessibility)}%</span></li>
-          <li>SEO: <span>${Math.round(data.metrics.seo)}%</span></li>
-          <li>carbon footprint: <span>${data.metrics.carbon.toFixed(3)}</span></li>
+      <a href="#">test</a>
+      <ul>
+        <li>Performace: <span>${data.metrics.performance}%</span></li>
+        <li>First contentful paint:<span>${data.metrics.firstContentfulPaint.toFixed(2)}Sec </span></li>
+        <li>Best practices: <span>${Math.round(data.metrics.bestPractices)}%</span></li>
+        <li>Accessibility:<span>${Math.round(data.metrics.accessibility)}%</span></li>
+        <li>SEO: <span>${Math.round(data.metrics.seo)}%</span></li>
+        <li>carbon footprint: <span>${data.metrics.carbon.toFixed(3)}</span></li>
       </ul>
       <p>${data.text}</p>
     </div>
