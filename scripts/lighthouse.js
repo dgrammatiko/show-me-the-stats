62b3c9bd6db5b3099a0560080503032fec8a91cf
dGrammatiko <d.grammatiko@gmail.com>
2020-08-30 15:10:56 +0200

    Fix lighthouse, re run fo 0-100

--- scripts/lighthouse.js
+++ scripts/lighthouse.js
@@ -9,6 +9,11 @@ if (!existsSync('./src_site/sitesData.json')) {
 if (!existsSync('./src_data')) {
   mkdirSync('./src_data')
 }
+if (!existsSync('./src_data_all')) {
+  mkdirSync('./src_data_all')
+}
+
+let lighthoseData;
 
 (async () => {
   const initialDataText = await readFile('./src_site/sitesData.json', { encoding: 'utf8' });
@@ -17,34 +22,23 @@ if (!existsSync('./src_data')) {
 
   for (const [index, site] of initialData.entries()) {
     const { href, host, pathname, protocol } = new url.parse(site.href)
-    if (100 >= index < 201) {
+    if (index < 1) {
       urlsForAudit.push(`${protocol}//${host}/`);
-      console.error(host.replace(/^www\./, '').replace(/\./g, '_'))
     }
   }
 
-  const lighthouseData = await PerfLeaderboard(urlsForAudit, 3, { launchOptions: {} });
+  lighthouseData = await PerfLeaderboard(urlsForAudit, 3, { launchOptions: {} });
 
-  // lighthoseData.forEach(lh => {
-  //   initialData.forEach((data, idx) => {
-  //     if (lh.requestedUrl.includes(data.themeUrl)) {
-  //       carbonVal = (lh.weight.total / 1024 / 1024 / 1024) * 0.06 * 1000;
-  //       const tempCur = {
-  //         performance: (Math.round((lh.lighthouse.performance + Number.EPSILON) * 100) / 100) * 100,
-  //         bestPractices: (Math.round((lh.lighthouse.bestPractices + Number.EPSILON) * 100) / 100) * 100,
-  //         accessibility: (Math.round((lh.lighthouse.accessibility + Number.EPSILON) * 100) / 100) * 100,
-  //         seo: (Math.round((lh.lighthouse.seo + Number.EPSILON) * 100) / 100) * 100,
-  //         carbon: carbonVal.toFixed(3),
-  //         firstContentfulPaint: (Math.round((lh.firstContentfulPaint + Number.EPSILON) * 100) / 100),
-  //         firstMeaningfulPaint: (Math.round((lh.largestContentfulPaint + Number.EPSILON) * 100) / 100),
-  //         firstCPUIdle: (Math.round((lh.totalBlockingTime + Number.EPSILON) * 100) / 100),
-  //         interactive: (Math.round((lh.timeToInteractive + Number.EPSILON) * 100) / 100),
-  //       };
+  // Store each site data as a json file
+  lighthouseData.forEach(async (lh) => {
+    const { href, host, pathname, protocol } = new url.parse(lh.requestedUrl)
 
-  //       initialData[idx] = { ...data, ...tempCur }
-  //     }
-  //   })
-  // });
+    // Add carbon Footprint
+    lh.lighthouse.carbon = parseFloat((lh.weight.total / 1024 / 1024 / 1024) * 0.06 * 1000).toPrecision(3);
 
-  await writeFile('./src_data/sitesData-with-LH_100-200.json', lighthouseData, { encoding: 'utf - 8' });
+    await writeFile(`./src_data/${host.replace(/^www\./, '').replace(/\./g, '_')}.json`, JSON.stringify(lh), { encoding: 'utf8' });
+  });
+
+  // @todo remove these files
+  await writeFile('./src_data_all/sitesData-with-LH_0-100.json', JSON.stringify(lighthouseData), { encoding: 'utf8' });
 })()
